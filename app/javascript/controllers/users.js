b92d9ec304e3ee6ac7232474299af1ddf051009e
CFCaserotto <carlos.caserotto@gamil.com>
2024-03-13 13:08:59 -0300

    Add JS Users Directory

---
+++ app/javascript/controllers/users.js
@@ -0,0 +1,23 @@
+import AppCtrl from "./app"
+
+export default class UsersCtrl extends AppCtrl {
+  constructor() {
+    super()
+
+    this.searchForm = find('.user_search')
+    this.searchInput = find('.search-input')
+    this.currentValue = this.searchInput.value
+
+    if (!this.isMobile) this.searchInput.focus()
+  }
+
+  search() {
+    setTimeout(() => {
+      const newValue = this.searchInput.value
+      
+      if (this.currentValue != newValue)
+        this.currentValue = newValue
+        submit(this.searchForm)
+    }, 500)
+  }
+}
